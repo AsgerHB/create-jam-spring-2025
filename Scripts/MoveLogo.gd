33fa32f2d37aa030ee3fb65273cfc8554ce0a7e1
BicaniWolfie <mathiasj20@live.dk>
2025-03-15 23:43:14 +0100

    Added logo
    Added an animated logo for the game in the main menu.

---
+++ Scripts/MoveLogo.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+
+var is_down: bool = false
+
+func _ready() -> void:
+	while true:
+		if is_down:
+			position.y -= 16
+		else:
+			position.y += 16
+		is_down = !is_down
+		await get_tree().create_timer(1.4166667).timeout
