ddb132be107500de2a5bbe57f53810051c36bcc1
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2025-03-16 11:13:23 +0100

    Add "exit" button in main menu

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -13,3 +13,7 @@ func _on_credits_button_pressed() -> void:
 
 func _on_instructions_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Instructions1.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
