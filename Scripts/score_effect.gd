28940a764f941ea8a22f7d6879a5b50af672813c
NicEastvillage <nicoesterby@gmail.com>
2025-03-26 21:14:35 +0100

    Adjust score effect

--- Scripts/score_effect.gd
+++ Scripts/score_effect.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var rigid_body: RigidBody2D
 var label: Label
-const time_alive: float = 2.0
+const time_alive: float = 1.0
 var time_left_alive: float = time_alive
 
 func _ready() -> void:
@@ -13,7 +13,7 @@ func _ready() -> void:
 
 func set_score(score: int):
 	label.text = "+" + str(score)
-	label.add_theme_color_override("font_color", Color.WHITE.lerp(Color.MAGENTA, clampf(score / 50.0, 0.0, 1.0)))
+	label.add_theme_color_override("font_color", Color.YELLOW.lerp(Color.INDIGO, clampf(score / 50.0, 0.0, 1.0)))
 	label.add_theme_font_size_override("font_size", lerpf(34.0, 72.0, clampf(score / 50.0, 0.0, 1.0)))
 	
 
@@ -23,4 +23,4 @@ func _process(delta: float) -> void:
 		queue_free()
 		return
 	
-	label.modulate.a = time_left_alive / time_alive
+	label.modulate.a = ease(time_left_alive / time_alive, 0.33)
