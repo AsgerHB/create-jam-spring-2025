61afebf0373a0476c5941d7f4d48cd59175265d4
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2025-03-26 21:42:22 +0100

    Upscale explosion, lightning and background for Luna

--- Scripts/background.gd
+++ Scripts/background.gd
@@ -10,4 +10,4 @@ func set_filled(filledness: float):
 			var a = float(y + 7) / (TetrisGame.HEIGHT + 7)
 			var b = filledness
 			var score = (a - b) * 3 + float((x ^ y) % 10) / 10
-			set_cell(Vector2i(x, y), 1, _get_tile_offset_from_score(score))
+			set_cell(Vector2i(x, y), 0, _get_tile_offset_from_score(score))
