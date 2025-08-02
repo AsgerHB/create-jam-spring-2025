ba89de2f241931666f2823ae657a47ba90c8e7ef
Asger Horn Brorholt <asgerhb@vivaldi.net>
2025-08-02 11:24:54 +0200

    Add 7 new levels

--- Scripts/MapSelector.gd
+++ Scripts/MapSelector.gd
@@ -6,15 +6,17 @@ static var empty = preload("res://Prefabs/Maps/empty.tscn")
 # Tip: You can select-and-drag new maps to an empty line, to get their paths.
 static var maps:Array[PackedScene] = [
 		preload("res://Prefabs/Maps/pyramid.tscn"),
-		preload("res://Prefabs/Maps/balloons.tscn"),
 		preload("res://Prefabs/Maps/checker.tscn"),
 		preload("res://Prefabs/Maps/clock.tscn"),
+		preload("res://Prefabs/Maps/compressed.tscn"),
 		preload("res://Prefabs/Maps/concrete1.tscn"),
 		preload("res://Prefabs/Maps/concrete2.tscn"),
 		preload("res://Prefabs/Maps/concrete3.tscn"),
 		preload("res://Prefabs/Maps/concrete4.tscn"),
+		preload("res://Prefabs/Maps/concrete5.tscn"),
 		preload("res://Prefabs/Maps/cube.tscn"),
 		preload("res://Prefabs/Maps/diamond.tscn"),
+		preload("res://Prefabs/Maps/dune.tscn"),
 		preload("res://Prefabs/Maps/funnel.tscn"),
 		preload("res://Prefabs/Maps/mole.tscn"),
 		preload("res://Prefabs/Maps/monster_cage.tscn"),
@@ -22,12 +24,17 @@ static var maps:Array[PackedScene] = [
 		preload("res://Prefabs/Maps/mult.tscn"),
 		preload("res://Prefabs/Maps/plant1.tscn"),
 		preload("res://Prefabs/Maps/plant2.tscn"),
+		preload("res://Prefabs/Maps/plant3.tscn"),
 		preload("res://Prefabs/Maps/pyramid.tscn"),
+		preload("res://Prefabs/Maps/rows.tscn"),
 		preload("res://Prefabs/Maps/sand.tscn"),
+		preload("res://Prefabs/Maps/sand_pillars.tscn"),
+		preload("res://Prefabs/Maps/squiggle.tscn"),
 		preload("res://Prefabs/Maps/wedge.tscn"),
 	]
 
 static func get_random_map(probability_empty) -> Map:
+	return preload("res://Prefabs/Maps/monster_mash.tscn").instantiate()
 	if randf() < probability_empty:
 		return empty.instantiate()
 	else:
